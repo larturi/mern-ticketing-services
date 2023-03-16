84d48ab2786b7cfb107f099107d38851c95ab1a9
Leandro Arturi <lea.arturi@gmail.com>
2023-03-16 13:51:29 -0300

    Auth Routers

---
+++ node-auth-service/src/routes/signup.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+
+const router = express.Router();
+
+router.post('/api/users/signup', [
+  body('email').isEmail().withMessage('Please provide a valid email'),
+  body('password').trim().isLength({ min: 6, max: 20 })
+    .withMessage('Password must to be 6 to 20 characters'),
+], 
+(req: Request, res: Response) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(404).send(errors.array())
+    }
+
+    const { email, password } = req.body;
+
+    console.log('Creating user...');
+
+    res.send({
+        email,
+        password
+    });
+})
+
+export { router as signUpRouter }
