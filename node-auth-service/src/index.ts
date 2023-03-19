2c4b599894a4c445009e871aaa1d7556541c54a8
Leandro Arturi <lea.arturi@gmail.com>
2023-03-19 10:16:03 -0300

    Signup

--- node-auth-service/src/index.ts
+++ node-auth-service/src/index.ts
@@ -1,10 +1,11 @@
 import  express  from 'express';
 import { json } from 'body-parser';
+
 import { currentUserRouter } from './routes/current-user';
 import { signInRouter } from './routes/signin';
 import { signOutRouter } from './routes/signout';
 import { signUpRouter } from './routes/signup';
-
+import { errorhandler } from './middlewares/error-handler';
 
 const app = express();
 app.use(json());
@@ -13,6 +14,8 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
+
+app.use(errorhandler);
 
 app.listen(3000, () => {
     console.log('Listening on port 3000!');
--- node-auth-service/src/routes/signup.ts
+++ node-auth-service/src/routes/signup.ts
@@ -13,7 +13,7 @@ router.post('/api/users/signup', [
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-        return res.status(404).send(errors.array())
+        throw new Error('Invalid email or password')
     }
 
     const { email, password } = req.body;
