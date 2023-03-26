75a0e29b1be2acefbc04aefead0419976bfb22a5
Leandro Arturi <lea.arturi@gmail.com>
2023-03-26 15:01:15 -0300

    Middleware validateRequest

--- node-auth-service/src/routes/signin.ts
+++ node-auth-service/src/routes/signin.ts
@@ -1,9 +1,19 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-    return res.send('SignIn')
+router.post('/api/users/signin', [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password')
+        .trim()
+        .notEmpty()
+        .withMessage('Password must be provided'),
+], 
+validateRequest,
+(req: Request, res: Response) => {
+
 })
 
 export { router as signInRouter }
--- node-auth-service/src/routes/signup.ts
+++ node-auth-service/src/routes/signup.ts
@@ -1,9 +1,9 @@
 import express, { Request, Response } from 'express';
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
-import { RequestValidationError } from '../errors/request-validation-error';
 import { User } from '../models/user';
 import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest } from '../middlewares/validate-request';
 
 const router = express.Router();
 
@@ -12,13 +12,8 @@ router.post('/api/users/signup', [
   body('password').trim().isLength({ min: 6, max: 20 })
     .withMessage('Password must to be 6 to 20 characters'),
 ], 
+validateRequest,
 async (req: Request, res: Response) => {
-
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-        throw new RequestValidationError(errors.array());
-    }
 
     const { email, password } = req.body;
 
