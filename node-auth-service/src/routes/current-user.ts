d460db095efdb9e7bddc65bed3ed6653ec0248bc
Leandro Arturi <lea.arturi@gmail.com>
2023-03-29 23:11:45 -0300

    Middleware RequireAuth

--- node-auth-service/src/routes/current-user.ts
+++ node-auth-service/src/routes/current-user.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express';
-import jwt from 'jsonwebtoken';
 import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router();
 
-router.get('/api/users/currentuser', currentUser, (req: Request, res: Response) => {
+router.get(
+    '/api/users/currentuser', 
+    currentUser, 
+    (req: Request, res: Response) => {
     res.send({ currentUser: req.currentUser || null });
 });
 
