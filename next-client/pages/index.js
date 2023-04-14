bbe65f57fc02e4751ce98224837ed7f2054fac23
Leandro Arturi <lea.arturi@gmail.com>
2023-04-14 13:27:44 -0300

    Check Cookie Auth on LandingPage

--- next-client/pages/index.js
+++ next-client/pages/index.js
@@ -1,25 +1,14 @@
-import React from 'react';
-import axios from 'axios';
+import BuildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
    console.log('User', currentUser);
    return <div>Index Page!</div>;
 };
 
-LandingPage.getInitialProps = async ({req}) => {
-   if (typeof window === 'undefined') {
-      // We are on the server
-      const { data } = await axios.get("http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-{
-    headers: req.headers,
-});
-      return data;
-   } else {
-      // We are on the browser
-      const { data } = await axios.get('/api/users/currentuser');
-      return data;
-   }
-   return {};
+LandingPage.getInitialProps = async (context) => {
+   const client = BuildClient(context);
+   const { data } = await client.get('/api/users/currentuser');
+   return data;
 };
 
 export default LandingPage;
